d4a52af7c80f927d1578287b1dd9bdfe2a7468f2
Aaron Jones <aaronjjj95@aol.com>
2024-07-24 20:11:55 +0000

    003 Added the start of JS to run game

---
+++ index.js
@@ -0,0 +1,11 @@
+function PlayerRoll (roll) {
+    const statement = 
+    document.getElementById("statement");
+    const playerChoice =
+    document.getElementById("playerChoice");
+    const computerChoice =
+    document.getElementById("computerChoice");
+    const randomNumber =
+    Math.floor(Math.random() * 3);
+
+}
